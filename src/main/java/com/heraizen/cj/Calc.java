d1cd326057111a216ac8daf682ba88483b43cd13
avulalakshman <lakshman.avula@outlook.com>
2019-10-18 17:57:16 +0530

    Update Calc.java
    added comments about sum method

--- src/main/java/com/heraizen/cj/Calc.java
+++ src/main/java/com/heraizen/cj/Calc.java
@@ -7,6 +7,7 @@ public class Calc{
 		public int add(int a, int b) {
 			return a + b;
 		}
+		// This method return the sum of the given integer array
 		public int sum(Integer arr[]) {
 			return Arrays.asList(arr).stream().reduce(0,Integer::sum);
 			
