8722771a9f9fa61396e0ac23b8c7df5b55c6ec7a
lakshman.avula@outlook.com <lakshman.avula@outlook.com>
2019-10-18 17:29:31 +0530

    Added logic to find add and sum methods

--- src/main/java/com/heraizen/cj/Calc.java
+++ src/main/java/com/heraizen/cj/Calc.java
@@ -1,5 +1,13 @@
 package com.heraizen.cj;
 
+import java.util.Arrays;
+
 public class Calc{
    
+		public int add(int a, int b) {
+			return a + b;
+		}
+		public int sum(Integer arr[]) {
+			return Arrays.asList(arr).stream().reduce(0,Integer::sum);
+		}
 }
