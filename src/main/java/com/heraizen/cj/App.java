a3539ee61c5a33304bb39a70dc9d964f0f22313e
lakshman.avula@outlook.com <lakshman.avula@outlook.com>
2019-10-18 17:00:57 +0530

    Special greetings are added

--- src/main/java/com/heraizen/cj/App.java
+++ src/main/java/com/heraizen/cj/App.java
@@ -1,7 +1,7 @@
 package com.heraizen.cj;
 public class App {
     public String getGreeting(String name) {
-        return "Hello : "+name;
+        return "Hello : "+name +"how are you?";
     }
 
     public static void main(String[] args) {
